2e9cf34e5f4edd4778f47abf2fe650a83e48200d
marochat <mamiyanx@gmail.com>
2021-07-13 07:19:25 +0900

    --days option

--- blockset.py
+++ blockset.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
+import sys,os,glob
 import argparse
 import re
 import numpy as np
 #import xml.etree.ElementTree as ET
+from datetime import datetime, timezone, timedelta, tzinfo
 from lxml import etree as ET
 import subprocess
 # logrottateのgzipファイルに対応するため
@@ -30,9 +31,12 @@ ipset_ufw = "/etc/ufw/before.rules"
 ipset_start_line = "blacklist-ipset-S"
 ipset_end_line = "blacklist-ipset-E"
 
+tz_jst = timezone(timedelta(hours=9), name='JST')
+
 # 関数定義
-# リスト中の重複数を集計して、データと登場回数のタプルのリストを作成して返す
 def array_count(array):
+    """リスト中の重複数を集計して、データと登場回数のタプルのリストを作成して返す
+    """
     dat = {}
     for i in array:
         dat[i] = dat[i] + 1 if i in dat.keys() else 1
@@ -40,6 +44,11 @@ def array_count(array):
 
 # IPV4形式のデータを扱うクラス、ネットマスクがあれば/ビット数で保管
 class ipv4adrset:
+    """
+    IPV4形式のデータを扱うクラス、xxx.xxx.xxx.xxx or xxx.xxx.xxx.xxx/m
+    __eq__(): ネットマスクのビットのみで比較
+            例) 192.168.0.5 と 192.168.0.0/24 は等しいとする
+    """
     def __init__(self):
         self.ips = []
         self.msk = 32
@@ -53,13 +62,13 @@ class ipv4adrset:
         else:
             res = re.match('^([0-9\.]+)\/([0-9]+)$', line)
             (ip, msk0) = (res.group(1), res.group(2)) if res else (line, 32)
-            self.ips = list(map(lambda n: int(n), ip.split('.')))
+            self.ips = [int(n) for n in ip.split('.')]
             self.msk = int(msk0)
 
     def set_mask(self, mask):
         if self.msk < mask:
             return self
-        msks = list(map(lambda n: int('1'*n+'0'*(8-n), 2), [8 if (mask - i*8) >= 8 else (mask - i*8) if (mask - i*8) > 0 else 0 for i in range(4)]))
+        msks = [int('1'*n+'0'*(8-n), 2) for n in [8 if (mask - i*8) >= 8 else (mask - i*8) if (mask - i*8) > 0 else 0 for i in range(4)]]
         rip = list(np.array(self.ips) & np.array(msks))
         ret = ipv4adrset()
         ret.ips = rip
@@ -67,8 +76,8 @@ class ipv4adrset:
         return ret
 
     def __eq__(self, other):
-        smsks = list(map(lambda n: int('1'*n+'0'*(8-n), 2), [ 8 if (self.msk - i*8) >= 8 else (self.msk - i*8) if (self.msk - i*8) > 0 else 0 for i in range(4)]))
-        omsks = list(map(lambda n: int('1'*n+'0'*(8-n), 2), [ 8 if (other.msk - i*8) >= 8 else (other.msk - i*8) if (other.msk - i*8) > 0 else 0 for i in range(4)]))
+        smsks = [int('1'*n+'0'*(8-n), 2) for n in [ 8 if (self.msk - i*8) >= 8 else (self.msk - i*8) if (self.msk - i*8) > 0 else 0 for i in range(4)]]
+        omsks = [int('1'*n+'0'*(8-n), 2) for n in [ 8 if (other.msk - i*8) >= 8 else (other.msk - i*8) if (other.msk - i*8) > 0 else 0 for i in range(4)]]
         sip = list(np.array(self.ips) & np.array(smsks) & np.array(omsks))
         oip = list(np.array(other.ips) & np.array(smsks) & np.array(omsks))
         return sip == oip
@@ -76,15 +85,21 @@ class ipv4adrset:
     def __str__(self):
         return '.'.join(map(lambda i: str(i), self.ips)) + ('/' + str(self.msk) if self.msk < 32 else '')
 
+    def __hash__(self):
+        return self.ips[0] * 0x1000000 + self.ips[1] * 0x10000 + self.ips[2] * 0x100 + self.ips[3]
+
 def int_tuple(str):
+    """argparseで整数タプルの引数を読み取る関数"""
     t = tuple(map(lambda x: int(x), str.split(',')))
     return t
 
 def ipv4adr(str):
+    """ipv4adrset型の引数を読み取る関数"""
     a = ipv4adrset(str)
     return a
 
 def readfile_info(readfunc):
+    """読み出すファイル名をログ出力するデコレータ"""
     def wrapper(*arg):
         logger.info('use logfile : %s', arg[0])
         return readfunc(*arg)
@@ -92,26 +107,30 @@ def readfile_info(readfunc):
 
 # mail.log から不正アクセスらしいIPアドレスを、リストに追加して返す
 def pattern_func_maillog(lst, line):
-    res = re.match('^.+\[([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\].+ SASL [A-Z]+ authentication failed.*$', line)
-    if res:
+    if res := re.match('^.+\[([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\].+ SASL [A-Z]+ authentication failed.*$', line):
         lst.append(res.group(1))
-    res = re.match('SSL_accept .+\[([0-9.]+)\]', line)
-    if res:
+    if res := re.match('SSL_accept .+\[([0-9.]+)\]', line):
         lst.append(res.group(1))
     return lst
 
 # ログファイルから不正アクセスらしいIPアドレスを取り出してリスト化する（gzipファイルはgzipモジュールのOPENを使用する）
 # 切り出しパターンの分離バージョン
-@readfile_info
-def get_iiplist(logfile, pattern_func):
+def get_iiplist(lfile, st, ed, pattern_func):
+    mlogs = []
+    if lfile is not None:
+        mlogs = [lfile]
+    else:
+        mlogs = [x for x in glob.glob(maillog_default + '*') if re.match('.*(log|gz|[0-9])',x) and os.path.getmtime(x) > st.timestamp()]
+        mlogs.sort(key=lambda x: os.path.getmtime(x))
     lst = []
-    try:
-        with open(logfile, 'r', encoding='utf-8') if not re.search('\.gz$', logfile) else gzip.open(logfile, 'rt') as fh:
-            for line in map(lambda ln: ln.rstrip('\n'), fh):
-                lst = pattern_func(lst, line)
-    except FileNotFoundError:
-        logger.error('file not found! : %s' % (logfile))
-    
+    for logfile in mlogs:
+        logger.info(logfile)
+        try:
+            with open(logfile, 'r', encoding='utf-8') if not re.search('\.gz$', logfile) else gzip.open(logfile, 'rt') as fh:
+                for line in map(lambda ln: ln.rstrip('\n'), fh):
+                    lst = pattern_func(lst, line)
+        except FileNotFoundError:
+            logger.error('file not found! : %s' % (logfile))
     return lst
 
 # 現在設定中のblocklistをリスト化
@@ -210,7 +229,7 @@ def set_blocklist_ufw(newips):
 def main():
     # オプション解析
     parser = argparse.ArgumentParser(description='blockset.py collection of unauthorized access list and restart firewall(ufw)')
-    parser.add_argument('logfile', nargs='?', help='input log file default:' + maillog_default, default=maillog_default)
+    parser.add_argument('logfile', nargs='?', help='input log file default:' + maillog_default, default=None)
     parser.add_argument('-n', '--none', action='store_true', help='Check only mode.')
     parser.add_argument('-f', '--force', action='store_true', help='bloclist force update, unless new record.')
     parser.add_argument('-d', '--debug', action='store_true', help='debug print mode')
@@ -218,6 +237,7 @@ def main():
     parser.add_argument('-o', '--orderck', type=int_tuple, metavar='MASK,COUNT', help='ipset order ckeck')
     parser.add_argument('-c', '--count', type=int, help='abuse count', default = 5)
     parser.add_argument('-a', '--address', type=ipv4adr, help='individually address entry to blacklist')
+    parser.add_argument('-D', '--days', type=int, help='days of collection', default=1)
 
     args = parser.parse_args()
 
@@ -233,7 +253,13 @@ def main():
         logger.info('mask bits : %s' % str(args.orderck[0])) 
         logger.info('count     : %s' % str(args.orderck[1])) 
 
-    siplist = get_iiplist(args.logfile, pattern_func_maillog)
+    now = datetime.now(tz_jst)
+    st = (now - timedelta(days=args.days))
+    ed = now
+
+    siplist = get_iiplist(args.logfile, st, ed, pattern_func_maillog)
+
+
 
     # IPアドレス、登場回数のタプルリストにする
     items = array_count(siplist)
